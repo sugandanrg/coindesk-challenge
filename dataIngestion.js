22684f7ff74a9b5ebc2feee83863348ded6f717b
Mohamed Zeeshan <sugandanrg@gmail.com>
2018-10-13 15:35:27 -0400

    Filter out invalid data

--- dataIngestion.js
+++ dataIngestion.js
@@ -1,11 +1,25 @@
 const csv = require('fast-csv');
+var sequelize = require('sequelize');
 var sourceData = [];
 
-csv
- .fromPath('testData-1539286153057.csv', {headers: true})
- .on('data', (data) =>
-   sourceData.push(data)
- )
- .on('end', () =>
- console.log(sourceData)
- );
+
+function dataIngestion(){
+  csv
+   .fromPath('testData-1539286153057.csv', {headers: true})
+   .on('data', (data) =>
+     sourceData.push(data)
+   )
+   .on('end', () => {
+   console.log(sourceData.length + " records loaded");
+   dataCleaning();
+   });
+}
+
+function dataCleaning(){
+  for (var i = 0; i < sourceData.length; i++)
+      if(sourceData[i].data != '' && 
+         sourceData[i].data > 0)
+        console.log(sourceData[i]);
+}
+
+dataIngestion();
