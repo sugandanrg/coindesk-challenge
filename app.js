3a177274cf8b0c2fed8a24f6a058596cf9ede1d3
Mohamed Zeeshan <sugandanrg@gmail.com>
2018-10-16 14:41:57 -0400

    intermediate check-in | async to be implemented

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const csv = require('fast-csv');
 const moment = require('moment');
 const mongoose = require('mongoose');
+const async = require('async');
 const stageData = require('./models/stageData.js');
 const coinData = require('./models/coinData.js');
 
 var sourceData = [];
 var validData = [];
+var testData = [];
 //mongoDB connection string
 const uri = "mongodb+srv://sugandanrg:temp1234@cluster0-hd2ly.mongodb.net/coindesk";
 mongoose.Promise = global.Promise;
@@ -46,11 +48,93 @@ function createStageModel(){
   stageData.collection.insertMany(validData, function(err, res) {
     if(err){ console.log(err);}
     console.log("VALID DATA: " + validData.length + " records loaded");
-    dataTransformation();
+    //dataTransformation();
   });
 }
 
 function dataTransformation(){
+  mongoose.connect(uri);
+  stageData.aggregate([
+    {
+      $group: {
+          _id: 'createdAt',
+          minDate: {$min: '$createdAt'},
+          maxDate: {$max: '$createdAt'}
+      }
+    }
+  ], function(err, res){
+        if(err) { console.log(err); return; }
+
+        var minDate = moment(String(res[0].minDate)).format('YYYY-MM-DD HH:mm:ss');
+        var maxDate = moment(String(res[0].maxDate)).format('YYYY-MM-DD HH:mm:ss');
+
+        stageData.distinct("FactorConfigId", function (err, pFactorConfigId ){
+            if(err) { console.log(err); return; }
+
+            //res.forEach(function(pFactorConfigId){
+                for(var f = 0; f < pFactorConfigId.length; f++ ){
+                stageData.find({
+                  FactorConfigId: pFactorConfigId[f]
+                }, function(err, res){
+                if(err) { console.log(err); return; }
+                //console.log("**********" + res[f].FactorConfigId + "**********");
+                var loopDate = minDate;
+                var i=0; var count = 0;
+                //if(FactorConfigId == 112){
+                while(getDateAlone(loopDate) <= getDateAlone(maxDate) && i < res.length - 1){
+                  //console.log("i " + i + " length " + res.length +  " createdDate " + res[i].createdAt +
+                  //"  loopD " + getDateAlone(loopDate) + " maxD "+ getDateAlone(maxDate));
+                        if(getDateAlone(res[i+1].createdAt) == getDateAlone(loopDate)){
+                      		loopDate = getDateTime(res[i+1].createdAt);
+                      		i += 1;
+                        }
+                        else{
+                        		  var temp= {
+                    						  id: res[i].id,
+                    						  data: res[i].data,
+                    						  FactorConfigId: res[i].FactorConfigId,
+                    						  createdAt: new Date(loopDate)
+                    						};
+                  						testData.push(temp);
+                              //console.log("else_else");
+                  						loopDate = addDate(loopDate,1);
+                        }
+                        if(loopDate >= maxDate){
+                            console.log(testData.length);
+                        /* console.log(testData.length);
+                        coinData.collection.insertMany(testData, function(err, res){
+                        if(err){ console.log(err); return; }
+                        //console.log(res);
+                      }); */
+                    }
+                  }
+
+                    function getDateAlone(getDate){
+                       var DateAlone = moment(String(getDate)).format('YYYY-MM-DD');
+                       return DateAlone;
+                    }
+                    function getDateTime(getDate){
+                      var Date_Time = moment(String(getDate)).format('YYYY-MM-DD HH:mm:ss');
+                      return Date_Time;
+                    }
+
+                    function addDate(getDate,i){
+                      var Add_Date = moment(String(moment(getDate).add(i, 'days'))).format('YYYY-MM-DD HH:mm:ss');
+                      return Add_Date;
+                    }
+              });
+            }
+          //});
+    });
+  });
+}
+
+
+dataTransformation();
+
+
+/*
+function aadataTransformation(){
   //transformation to attain recent coin marketcap in a day
     stageData.aggregate([
       {
@@ -75,7 +159,7 @@ function dataTransformation(){
       coinData.collection.insertMany(result)
       .then( function(res) {
         console.log("EXPECTED DATA: " + result.length + " records loaded");
-        stageData.collection.drop(); //drop staged collection
+        //stageData.collection.drop(); //drop staged collection
         mongoose.connection.close(); //closeMongoDBConn();
       })
       .catch(function(err){
@@ -84,6 +168,4 @@ function dataTransformation(){
     });
 }
 
-
-//data flow starts here
-dataIngestion();
+*/
